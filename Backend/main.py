dbfe2b8044513de87b14de6330848d7fcf4e7640
HerlanLima <herlanphs2@gmail.com>
2025-03-23 21:34:52 -0300

    Obras Autor

--- Backend/main.py
+++ Backend/main.py
@@ -93,16 +93,6 @@ def usuario_logado():
 
 
 
-
-
-
-#========================================================================================================
-
-
-
-
-
-
 #USUARIO AUTOR INICIO
 #=======================================================================================================
 @app.route('/verificar_autor', methods=['POST'])
@@ -148,8 +138,6 @@ def verificar_autor():
     return jsonify(pessoa)
 
 
-
-
 @app.route('/autor-logado', methods=['GET'])
 def Autor_logado():
     IdAutor = session.get('IdAutor')
@@ -163,7 +151,7 @@ def Autor_logado():
     cursor = conexao.cursor(dictionary=True)
     cursor.execute("SELECT * FROM Autor WHERE PessoaID = %s", (IdAutor))
     usuario = cursor.fetchone()
-
+    
     cursor.close()
     conexao.close()
 
@@ -171,11 +159,44 @@ def Autor_logado():
         return jsonify({"erro": "Usuário não encontrado"}), 404
 
     return jsonify(usuario)
-#=======================================================================================================
 
 
+@app.route('/obras-autor', methods=['GET'])
+def obras_autor():
+    IdAutor = session.get('IdAutor')
 
+    print("Id do Autor na sessão:", IdAutor) 
 
+    if not IdAutor:
+        return jsonify({"erro": "Usuário não autenticado"}), 401
+    
+    conexao = conectar_banco()
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM ObradeArte WHERE AutorID = %s", (IdAutor))
+    obras_de_arte = curso.fetchall()
+    colecao = {"colecao":[]}
+
+    for obj in range(0, len(obras_de_arte)):
+        colecao["colecao"][obj] = {
+            "imagem": obras_de_arte[obj][1],
+            "tipo_arquivo": obras_de_arte[obj][2],
+            "titulo": obras_de_arte[obj][3],
+            "descricao": obras_de_arte[obj][4],
+            "data_publicacao": obras_de_arte[obj][5],
+            "estilo_arte": obras_de_arte[obj][6],
+            "pais_galeria":obras_de_arte[obj][7],
+            "altura": obras_de_arte[obj][8],
+            "largura": obras_de_arte[obj][9],
+            "valor": obras_de_arte[obj][10],
+            "status": obras_de_arte[obj][11] #Não sei se vai preicsar!
+        }
+
+    cursor.close()
+    conexao.close()
+
+    return jsonify(usuario)
+
+        
 #=======================================================================================================
 #USUARIO AUTOR FIM
 
